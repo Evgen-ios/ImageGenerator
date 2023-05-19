81dd6f0fe77bfd07ed830c2ab510ea6e80f1aba4
Evgeniy Goncharov <jack38irk@icloud.com>
2023-05-19 16:06:17 +0800

    Initial Commit

---
+++ Image Generator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Image Generator
+//
+//  Created by Evgeniy Goncharov on 19.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
